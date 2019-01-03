9222c47dfc4abdbb2fe222a201d510a3372549f3
Nícolas F. R. A. Prado <nfraprado@protonmail.com>
2019-01-03 12:42:11 -0200

    Clean get_address_nonce

--- blockchain.py
+++ blockchain.py
@@ -1,20 +1,22 @@
 import requests
 import logging
-import json
 
 GASPRICE = 32000000000
 GASLIMIT = 500000
+
 
 def get_address_nonce(address):
     query_url = 'https://rinkeby.etherscan.io/api?module=proxy&action=eth_getTransactionCount'
     query_url += '&address={0}&tag=latest'.format(address)
     response = requests.post(query_url)
-    logging.debug('Got response from address nonce query: {0}'.format(response.text))
+    logging.debug('Got response from address nonce query: {0}'.format(
+        response.text))
 
     if response.status_code == requests.codes.ok:
         nonce_hex = response.json().get('result', None)
         return int(nonce_hex, 16)
-    return None
+    else:
+        return None
 
 
 def get_transactions_on_address(address):
